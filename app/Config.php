4b3ed3e2c4820a8ac216ff57eb70ecaead7d47e9
HelleboidQ <quentin.helboid@hotmail.fr>
2016-04-19 11:30:18 +0200

    Ajout Header

--- app/Config.php
+++ app/Config.php
@@ -30,12 +30,12 @@ class Config
         /**
          * Define the complete site URL.
          */
-        define('SITEURL', 'http://localhost/WordPress-en-mieux/');
+        define('SITEURL', 'http://localhost/basenova/');
 
         /**
          * Define relative base path.
          */
-        define('DIR', '/WordPress-en-mieux/');
+        define('DIR', '/basenova/');
 
         /**
          * Set the Application Router.
@@ -101,7 +101,7 @@ class Config
         /**
          * Optional create a constant for the name of the site.
          */
-        define('SITETITLE', 'BASIS');
+        define('SITETITLE', 'WordPress V2');
 
         /**
          * Optional set a site email address.
