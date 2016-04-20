3be5166c79b0506d12b2ab35051115abeb21744b
Leperu Jonathan <jonathan_leperu@ens.univ-artois.fr>
2016-04-20 08:56:41 +0200

    Manage admin

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -11,9 +11,15 @@ namespace App\Controllers;
 
 use App\Models\Queries\ArticleSQL;
 use App\Models\Queries\CategorieSQL;
+use App\Models\Queries\CommentaireSQL;
 use App\Models\Queries\UserSQL;
+use App\Models\Tables\Article;
+use App\Models\Tables\Categorie;
+use App\Models\Tables\Commentaire;
+use App\Models\Tables\User;
 use Core\View;
 use Core\Controller;
+use Helpers\DB\EntityManager;
 use Helpers\Session;
 use Helpers\Twig;
 use Helpers\Url;
@@ -84,6 +90,81 @@ class Admin extends Controller
     }
 
     function manageComment(){
+
+    }
+    
+    function addCategorie(){
+
+        if(Session::get('admin') != 1)
+            Url::redirect('utilisateur/login');
+
+        if(isset($_POST['ajouter'])){
+
+            $table_categorie = new Categorie($_POST['titre']);
+            EntityManager::getInstance()->save($table_categorie);
+            Session::set('message', 'Vous avez ajouté une nouvelle catégorie');
+            Url::redirect('admin/categories');
+
+        }
+        Url::redirect();
+    }
+
+    function addArticle(){
+
+        if(Session::get('admin') != 1)
+            Url::redirect('utilisateur/login');
+
+        if(isset($_POST['ajouter'])){
+
+            // URL A FAIRE
+
+            $table_article = new Article(Session::get('id'),$_POST['categorie'],$_POST['titre'],$_POST['contenu'],"",date("Y-m-d"));
+            EntityManager::getInstance()->save($table_article);
+            Session::set("message","Article ajouter");
+            Url::redirect('admin/articles');
+        }
+        Url::redirect();
+    }
+
+    function deleteUser($id){
+
+        if(Session::get('admin') != 1)
+            Url::redirect('utilisateur/login');
+
+
+        if($id != Session::get('id')){
+
+            $commentaireSQL = new CommentaireSQL();
+            $commentaires = $commentaireSQL->prepareFindWithCondition("idUser = :idu" , array(':idu',$id));
+
+            // delete tous les commentaires associés à l'utilisateur
+            foreach ($commentaires as $c){
+                $comment = new Commentaire();
+                $comment->setId($c->id);
+                EntityManager::getInstance()->delete($comment);
+            }
+
+            // supprime l'utilisateur
+            $user = new User();
+            $user->setId($id);
+            EntityManager::getInstance()->delete($user);
+
+        }else{
+
+            // si c'est la personne qui execute la fonction
+            Session::set('message',"Vous ne pouvez pas vous supprimer");
+        }
+
+        Url::redirect('admin/users');
+    }
+
+    function deleteCategorie($id){
+
+
+
+    }
+
+    function deleteArticle($id){
 
     }
 }
