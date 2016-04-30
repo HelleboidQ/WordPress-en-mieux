467a56e2427ad98af126576fd402fc091fde0e27
HelleboidQ <quentin.helboid@hotmail.fr>
2016-04-30 22:33:14 +0200

    Modification des articles page d'index

--- app/Views/Article/index.php
+++ app/Views/Article/index.php
@@ -1,10 +1,24 @@
- 
-{% for article in articles %}
-<div class="col-md-12">
-    {{ article.titre }} - Le {{ article.date | date("d/m/Y") }} - Par {{ article.user.login }} - {{ article.categorie.titre }}
-    <br />
-    {{ article.contenu | slice(0,46) }}...  
-    <br />
-    <a href="{{url}}article/{{ article.id }}" > Plus ... </a> 
+<div class="allposts">
+    {% for article in articles %}    
+    <article>
+        <a href="{{url}}article/{{ article.id }}" > 
+            <img src="{{url}}assets/images/{{ article.image }}" alt="{{ article.titre }}">
+            <div class="degpost"></div>
+            <div class="titlepost">
+                <h3>{{ article.titre }}</h3>
+            </div>
+            <div class="infospost">
+                <span class="datepost">
+                    <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Le {{ article.date | date("d/m/Y") }}
+                </span>
+                <span class="catpost">
+                    <i class="glyphicon glyphicon-tags" aria-hidden="true"></i>{{ article.categorie.titre }}
+                </span>
+            </div>
+            <div class="extrait">
+                <p>{{ article.contenu | slice(0,230) }}...</p>
+            </div>
+        </a>
+    </article>  
+    {% endfor %}
 </div>
-{% endfor %}
